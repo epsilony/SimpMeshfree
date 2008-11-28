73bf0403a1178486b4eae7cae6fd7708377b4014
svnuser <svnuser@b9806fc4-da95-481b-a3ce-6258aed95de6>
2008-11-28 01:35:09 +0000

    首次进入版本库，什么也没干几乎
    git-svn-id: svn://epsilony.net/epsilonRepos/old/MeshfreeRepos/trunk@39 b9806fc4-da95-481b-a3ce-6258aed95de6

---
+++ src/net/epsilony/simpmeshfree/geom/Node.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.simpmeshfree.geom;
+
+/**
+ *
+ * @author epsilon
+ */
+public class Node {
+
+    double x, y;
+
+    public Node(double xGive, double yGive) {
+        x = xGive;
+        y = yGive;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX() {
+    }
+
+    public void setY() {
+    }
+}
---
+++ src/net/epsilony/simpmeshfree/geom/NodeBucket.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.simpmeshfree.geom;
+import java.util.List;
+/**
+ *
+ * @author epsilon
+ */
+public class NodeBucket {
+
+    double xMin, yMin, xMax, yMax;
+    int num;
+    List<Node> node;
+    int MAX = 1;
+}
