2f7c179db8aa2939ffefdf799a18d36de0024f32
epsilon <epsilon@epsilon-TA790GX-128M>
2012-05-30 09:33:42 +0800

    Just before testing whole WeakformProcessor2D, the features are freezed temprorily in order to desing the simpmeshfree_gui python project.

--- src/net/epsilony/simpmeshfree/model2d/test/WeakformProcessor2DDemoUtils.java
+++ src/net/epsilony/simpmeshfree/model2d/test/WeakformProcessor2DDemoUtils.java
@@ -20,16 +20,19 @@ import no.uib.cipr.matrix.DenseMatrix;
  * @author epsilon
  */
 public class WeakformProcessor2DDemoUtils {
-    public static class MLSShapeFunctionFactory implements ShapeFunctionFactory{
+
+    public static class MLSShapeFunctionFactory implements ShapeFunctionFactory {
+
         GeomUtils geomUtils;
         WeightFunctionCore coreFun;
         int baseOrder;
-        int minNdNum,maxNdNum;
-        private Object[] genArgs(){       
-            SupportDomainCritierion critierion=geomUtils.new VisibleCritieron(minNdNum,maxNdNum);
-            WeightFunction weightFunction=WeightFunctions.factory(coreFun, critierion.getDistanceSquareFunction());
-            CoordinatePartDiffArrayFunction baseFun=Complete2DPolynomialBase.complete2DPolynomialBase(baseOrder);
-            Object[] results=new Object[]{weightFunction,baseFun,critierion};
+        int minNdNum, maxNdNum;
+
+        private Object[] genArgs() {
+            SupportDomainCritierion critierion = geomUtils.new VisibleCritieron(minNdNum, maxNdNum);
+            WeightFunction weightFunction = WeightFunctions.factory(coreFun, critierion.getDistanceSquareFunction());
+            CoordinatePartDiffArrayFunction baseFun = Complete2DPolynomialBase.complete2DPolynomialBase(baseOrder);
+            Object[] results = new Object[]{weightFunction, baseFun, critierion};
             return results;
         }
 
@@ -40,48 +43,78 @@ public class WeakformProcessor2DDemoUtils {
             this.minNdNum = minNdNum;
             this.maxNdNum = maxNdNum;
         }
-        
-        
+
         @Override
         public ShapeFunction factory() {
-            Object[] args=genArgs();
-            return new ShapeFunctions2D.MLS((WeightFunction)args[0],(CoordinatePartDiffArrayFunction)args[1],(SupportDomainCritierion)args[2]);
+            Object[] args = genArgs();
+            return new ShapeFunctions2D.MLS((WeightFunction) args[0], (CoordinatePartDiffArrayFunction) args[1], (SupportDomainCritierion) args[2]);
         }
     }
-    
-    public static MLSShapeFunctionFactory genShapeFunctionFactory(GeomUtils geomUtils, WeightFunctionCore coreFunc, int baseOrder, int minNdNum,int maxNdNum){
+
+    public static MLSShapeFunctionFactory genShapeFunctionFactory(GeomUtils geomUtils, WeightFunctionCore coreFunc, int baseOrder, int minNdNum, int maxNdNum) {
         return new MLSShapeFunctionFactory(geomUtils, coreFunc, baseOrder, minNdNum, maxNdNum);
     }
-    
-    public static WeakformProcessor2D weakformProcessor(GeomUtils geomUtils, WeightFunctionCore coreFunc, int baseOrder, int minNdNum,int maxNdNum,DenseMatrix constitutiveLaw,double penalty,WeakformProblem workProblem){
+
+    public static WeakformProcessor2D weakformProcessor(GeomUtils geomUtils, WeightFunctionCore coreFunc, int baseOrder, int minNdNum, int maxNdNum, DenseMatrix constitutiveLaw, double penalty, WeakformProblem workProblem) {
         MLSShapeFunctionFactory shapeFunFactory = genShapeFunctionFactory(geomUtils, coreFunc, baseOrder, minNdNum, maxNdNum);
-        int ndsSize=geomUtils.allNodes.size();
+        int ndsSize = geomUtils.allNodes.size();
         SimpAssemblier assemblier = new WeakformAssembliers2D.SimpAssemblier(constitutiveLaw, penalty, ndsSize);
         FlexCompRowMatrixSolver eqSolver = new EquationSolvers.FlexCompRowMatrixSolver(MatrixUtils.SPD);
-        return new WeakformProcessor2D(shapeFunFactory, assemblier,workProblem,eqSolver);
+        return new WeakformProcessor2D(shapeFunFactory, assemblier, workProblem, eqSolver);
     }
-    
-    public static WeakformProcessor2D timoshenkoBeam(double width,double height,double P,double E,double v,double lineSize,double spaceNdsDis,double penalty){
-        int power=2;
-        int baseOrder=2;
-        int minNdNum=15;
-        int maxNdNum=20;
+
+    public static WeakformProcessor2D timoshenkoBeam(double width, double height, double P, double E, double v, double lineSize, double spaceNdsDis, double penalty, Pipe pipe) {
+        int power = 2;
+        int baseOrder = 2;
+        int minNdNum = 15;
+        int maxNdNum = 20;
         DenseMatrix conLaw = ConstitutiveLaws2D.getPlaneStress(E, v);
-        WeightFunctionCore coreFun=new WeightFunctionCores.TriSpline();
-        RectangleModel rectModel=new RectangleModel(width, height, lineSize, spaceNdsDis);
+        WeightFunctionCore coreFun = new WeightFunctionCores.TriSpline();
+        RectangleModel rectModel = new RectangleModel(width, height, lineSize, spaceNdsDis);
         ArrayList<LineBoundary> bnds = rectModel.boundaries();
-        GeomUtils geomUtils=new GeomUtils(bnds, rectModel.spaceNodes(), 2);
-        WeakformProblem workProblem=new WeakformProblems2D.TimoshenkoExactBeamProblem(width, height, E, v, P, bnds, null, rectModel.quadrangles(), power);
+        GeomUtils geomUtils = new GeomUtils(bnds, rectModel.spaceNodes(), 2);
+        WeakformProblem workProblem = new WeakformProblems2D.TimoshenkoExactBeamProblem(width, height, E, v, P, bnds, null, rectModel.quadrangles(), power);
+        if (null != pipe) {
+            pipe.set(conLaw, coreFun, rectModel, geomUtils, workProblem);
+        }
         return weakformProcessor(geomUtils, coreFun, baseOrder, minNdNum, maxNdNum, conLaw, penalty, workProblem);
     }
-    
-    public static WeakformProcessor2D timoshenkoBeam(){
-        return timoshenkoBeam(48, 12, -1000, 3e7, 0.3, 2, 2, 1e8);
+
+    public static WeakformProcessor2D timoshenkoBeam(Pipe pipe) {
+        return timoshenkoBeam(48, 12, -1000, 3e7, 0.3, 2, 2, 1e8, pipe);
     }
-    
+
+    public static class Pipe {
+
+        DenseMatrix conLaw;
+        WeightFunctionCore coreFun;
+        RectangleModel rectModel;
+        GeomUtils geomUtils;
+        WeakformProblem workProblem;
+
+        public final void set(DenseMatrix conLaw, WeightFunctionCore coreFun, RectangleModel rectModel, GeomUtils geomUtils, WeakformProblem workProblem) {
+            this.conLaw = conLaw;
+            this.coreFun = coreFun;
+            this.rectModel = rectModel;
+            this.geomUtils = geomUtils;
+            this.workProblem = workProblem;
+        }
+
+        public Pipe(DenseMatrix conLaw, WeightFunctionCore coreFun, RectangleModel rectModel, GeomUtils geomUtils, WeakformProblem workProblem) {
+            set(conLaw, coreFun, rectModel, geomUtils, workProblem);
+        }
+
+        public Pipe() {
+        }
+    }
+    public static Pipe newPipe(){
+        return new Pipe();
+    }
     public static void main(String[] args) {
-        WeakformProcessor2D processor=timoshenkoBeam();
+        Pipe pipe =new Pipe();
+        WeakformProcessor2D processor = timoshenkoBeam(pipe);
         processor.process(1);
-        
+        processor.solveEquation();
+
     }
 }
