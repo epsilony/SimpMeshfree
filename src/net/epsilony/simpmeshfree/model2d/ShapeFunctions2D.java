9bdca4328d1f21f8c803e738a0d105976f8828a7
Man YUAN <epsilonyuan@gmail.com>
2012-06-04 22:53:15 +0800

    Change the linear solver of MLS algorithm in ShapeFunctions2D#MLS. The new linear solver  is the LU solver in EJML. Tests passed. Profiling (WeakformProcessorDemoUtils) shows that the solver is 5x fast.

--- src/net/epsilony/simpmeshfree/model2d/ShapeFunctions2D.java
+++ src/net/epsilony/simpmeshfree/model2d/ShapeFunctions2D.java
@@ -12,8 +12,10 @@ import net.epsilony.simpmeshfree.model.*;
 import net.epsilony.simpmeshfree.utils.BasesFunction;
 import static net.epsilony.simpmeshfree.utils.CommonUtils.len2DBase;
 import net.epsilony.utils.geom.Coordinate;
-import no.uib.cipr.matrix.DenseVector;
-import no.uib.cipr.matrix.UpperSymmDenseMatrix;
+import org.ejml.alg.dense.decomposition.lu.LUDecompositionAlt;
+import org.ejml.alg.dense.linsol.lu.LinearSolverLu;
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
 
 /**
  *
@@ -24,7 +26,7 @@ public class ShapeFunctions2D {
     public static int MAX_NODES_SIZE_ESTIMATION = 50;
 
     public static TDoubleArrayList[] initOutputResult(int partDiffOrder) {
-        return initOutputResult(null,  partDiffOrder,MAX_NODES_SIZE_ESTIMATION);
+        return initOutputResult(null, partDiffOrder, MAX_NODES_SIZE_ESTIMATION);
     }
 
     public static TDoubleArrayList[] initOutputResult(TDoubleArrayList[] result, int partDiffOrder, int ndsNum) {
@@ -55,12 +57,12 @@ public class ShapeFunctions2D {
         return result;
     }
 
-    public static void multAddTo(DenseVector gamma, ArrayList<TDoubleArrayList> B, TDoubleArrayList aim) {
-        int dimI = B.get(0).size(), dimJ = gamma.size();
+    public static void multAddTo(DenseMatrix64F gamma, ArrayList<TDoubleArrayList> B, TDoubleArrayList aim) {
+        int dimI = B.get(0).size(), dimJ = gamma.numRows;
         for (int i = 0; i < dimI; i++) {
             double t = aim.getQuick(i);
             for (int j = 0; j < dimJ; j++) {
-                t += gamma.get(j) * B.get(j).getQuick(i);
+                t += gamma.unsafe_get(j, 0) * B.get(j).getQuick(i);
             }
             aim.setQuick(i, t);
         }
@@ -70,6 +72,8 @@ public class ShapeFunctions2D {
      * 移动最小二乘法，没有通过白盒测试。测试样例证明了其的单位分解性和再生性。
      */
     public static class MLS implements ShapeFunction {
+        private DenseMatrix64F A_bak;
+        private LinearSolverLu luSolver;
 
         public MLS(WeightFunction weightFunction, BasesFunction basesFunction, SupportDomainCritierion criterion) {
             init(weightFunction, basesFunction, criterion);
@@ -79,20 +83,20 @@ public class ShapeFunctions2D {
         BasesFunction basesFunction;
         SupportDomainCritierion criterion;
         TDoubleArrayList[] nodesWeights = new TDoubleArrayList[3];
-        UpperSymmDenseMatrix A, A_x, A_y;
+        DenseMatrix64F A, A_x, A_y;
         ArrayList<TDoubleArrayList> B, B_x, B_y;
-        DenseVector p, p_x, p_y, gamma, gamma_x, gamma_y;
-        UpperSymmDenseMatrix[] As;
+        DenseMatrix64F p, p_x, p_y, gamma, gamma_x, gamma_y;
+        DenseMatrix64F[] As;
         List<ArrayList<TDoubleArrayList>> Bs;
         double[][] ps_arr;
-        private DenseVector tv;
+        private DenseMatrix64F tv;
 
         @Override
-        public TDoubleArrayList[] values(Coordinate center, Boundary centerBnd, TDoubleArrayList[] resultCache, ArrayList<Node> resNodes) {
+        public TDoubleArrayList[] values(Coordinate center, Boundary centerBnd, TDoubleArrayList[] shapeFunVals, ArrayList<Node> resNodes) {
 
             double supR = criterion.setCenter(center, centerBnd, resNodes);
             int ndsNum = resNodes.size();
-            TDoubleArrayList[] results = initOutputResult(resultCache, diffOrder, ndsNum);
+            TDoubleArrayList[] results = initOutputResult(shapeFunVals, diffOrder, ndsNum);
             int diffDim = len2DBase(diffOrder);
             int baseDim = basesFunction.getDim();
 
@@ -108,20 +112,20 @@ public class ShapeFunctions2D {
                     v.fill(0, ndsNum, 0);
                 }
             }
-            
+
             basesFunction.setDiffOrder(0);
             for (int diffDimIdx = 0; diffDimIdx < diffDim; diffDimIdx++) {
                 TDoubleArrayList weights_d = nodesWeights[diffDimIdx];
-                UpperSymmDenseMatrix A_d = As[diffDimIdx];
+                DenseMatrix64F A_d = As[diffDimIdx];
                 ArrayList<TDoubleArrayList> B_d = Bs.get(diffDimIdx);
                 double[] tp = ps_arr[0];
                 int ndIdx = 0;
                 for (Node nd : resNodes) {
                     basesFunction.values(nd, ps_arr);
-                    double weight_d=weights_d.getQuick(ndIdx);
+                    double weight_d = weights_d.getQuick(ndIdx);
                     for (int i = 0; i < baseDim; i++) {
                         double p_i = tp[i];
-                        for (int j = i; j < baseDim; j++) {
+                        for (int j = 0; j < baseDim; j++) {
                             double p_ij = p_i * tp[j];
                             A_d.add(i, j, weight_d * p_ij);
                         }
@@ -133,8 +137,15 @@ public class ShapeFunctions2D {
             }
             basesFunction.setDiffOrder(diffOrder);
             basesFunction.values(center, ps_arr);
+        
+            A_bak.set(A);
+            luSolver.setA(A_bak);
 
-            A.solve(p, gamma);
+            tv.set(p);
+            luSolver.solve(tv, gamma);
+
+//            CommonOps.solve(A, p, gamma);
+
             multAddTo(gamma, B, results[0]);
 
             if (diffOrder < 1) {
@@ -142,16 +153,15 @@ public class ShapeFunctions2D {
             }
 
             tv.zero();
-            A_x.mult(gamma, tv);
-            tv.scale(-1);
-            tv.add(p_x);
-            A.solve(tv, gamma_x);
-
+            CommonOps.mult(-1, A_x, gamma, tv);
+            CommonOps.add(p_x, tv, tv);
+            luSolver.solve(tv, gamma_x);
+//            CommonOps.solve(A, tv, gamma_x);
             tv.zero();
-            A_y.mult(gamma, tv);
-            tv.scale(-1);
-            tv.add(p_y);
-            A.solve(tv, gamma_y);
+            CommonOps.mult(-1, A_y, gamma, tv);
+            CommonOps.add(p_y, tv, tv);
+            luSolver.solve(tv,gamma_y);
+//            CommonOps.solve(A, tv, gamma_y);
 
             multAddTo(gamma_x, B, results[1]);
             multAddTo(gamma, B_x, results[1]);
@@ -186,10 +196,10 @@ public class ShapeFunctions2D {
             this.criterion = criterion;
             this.basesFunction = baseFunction;
             final int baseDim = baseFunction.getDim();
-            A = new UpperSymmDenseMatrix(baseDim);
-            A_x = new UpperSymmDenseMatrix(baseDim);
-            A_y = new UpperSymmDenseMatrix(baseDim);
-            As = new UpperSymmDenseMatrix[]{A, A_x, A_y};
+            A = new DenseMatrix64F(baseDim, baseDim);
+            A_x = new DenseMatrix64F(baseDim, baseDim);
+            A_y = new DenseMatrix64F(baseDim, baseDim);
+            As = new DenseMatrix64F[]{A, A_x, A_y};
 
             B = new ArrayList<>(baseDim);
             B_x = new ArrayList<>(baseDim);
@@ -202,17 +212,19 @@ public class ShapeFunctions2D {
                 }
             }
 
-            gamma = new DenseVector(baseDim);
-            gamma_x = new DenseVector(baseDim);
-            gamma_y = new DenseVector(baseDim);
+            gamma = new DenseMatrix64F(baseDim, 1);
+            gamma_x = new DenseMatrix64F(baseDim, 1);
+            gamma_y = new DenseMatrix64F(baseDim, 1);
 
             ps_arr = new double[3][baseDim];
 
-            p = new DenseVector(ps_arr[0], false);
-            p_x = new DenseVector(ps_arr[1], false);
-            p_y = new DenseVector(ps_arr[2], false);
+            p = DenseMatrix64F.wrap(ps_arr[0].length, 1, ps_arr[0]);
+            p_x = DenseMatrix64F.wrap(ps_arr[1].length, 1, ps_arr[1]);
+            p_y = DenseMatrix64F.wrap(ps_arr[2].length, 1, ps_arr[2]);
 
-            tv = new DenseVector(baseDim);
+            tv = new DenseMatrix64F(baseDim, 1);
+            luSolver=new LinearSolverLu(new LUDecompositionAlt());
+            A_bak= new DenseMatrix64F(baseDim,baseDim);
         }
     }
 }
