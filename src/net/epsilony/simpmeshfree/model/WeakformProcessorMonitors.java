240aadae57da4abcbc473daaef5556cbc8228d3c
Man YUAN <epsilonyuan@gmail.com>
2012-06-20 16:35:32 +0800

    Add a recorder monitor just for record balance assembly

--- src/net/epsilony/simpmeshfree/model/WeakformProcessorMonitors.java
+++ src/net/epsilony/simpmeshfree/model/WeakformProcessorMonitors.java
@@ -5,11 +5,15 @@
 package net.epsilony.simpmeshfree.model;
 
 import gnu.trove.list.array.TDoubleArrayList;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import net.epsilony.simpmeshfree.utils.CountableQuadraturePointIterator;
+import net.epsilony.simpmeshfree.utils.CommonUtils;
 import net.epsilony.simpmeshfree.utils.QuadraturePoint;
+import net.epsilony.simpmeshfree.utils.QuadraturePointIterator;
 import org.apache.log4j.Logger;
 
 /**
@@ -18,10 +22,68 @@ import org.apache.log4j.Logger;
  */
 public class WeakformProcessorMonitors {
 
-    public static class SimpLogger implements WeakformProcessorMonitor {
+    public static abstract class Adapter implements WeakformProcessorMonitor {
+
+        protected WeakformProcessor processor;
+        protected int coreNum;
+        protected ExecutorService executor;
+
+        @Override
+        public void avatorInited(WeakformAssemblier asm, ShapeFunction shapeFun, int asmId) {
+        }
+
+        @Override
+        public void balanceAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, VolumeCondition volBc, int asmId) {
+        }
+
+        @Override
+        public void dirichletAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, int asmId) {
+        }
+
+        @Override
+        public void neumannAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, int asmId) {
+        }
+
+        @Override
+        public void beforeProcess(ExecutorService executor, int coreNum) {
+            this.executor = executor;
+            this.coreNum = coreNum;
+        }
+
+        @Override
+        public void processStarted(ExecutorService executor) {
+        }
+
+        @Override
+        public void avatorUnited(WeakformAssemblier asm) {
+        }
+
+        @Override
+        public void beforeEquationSolve() {
+        }
+
+        @Override
+        public void equationSolved() {
+        }
+
+        @Override
+        public WeakformProcessor getProcessor() {
+            return processor;
+        }
+
+        @Override
+        public void setProcessor(WeakformProcessor processor) {
+            this.processor = processor;
+        }
+    }
+
+    public static WeakformProcessorMonitor simpLogger() {
+        return new SimpLogger();
+    }
+
+    public static class SimpLogger extends Adapter {
 
         Logger logger;
-        private WeakformProcessor processor;
         int avatorNum;
 
         @Override
@@ -30,38 +92,23 @@ public class WeakformProcessorMonitors {
         }
 
         @Override
-        public void balanceAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, VolumeCondition volBc, int asmId) {
-            
-        }
-
-        @Override
-        public void dirichletAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, int asmId) {
-            
-        }
-
-        @Override
-        public void neumannAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, int asmId) {
-            
-        }
-
-        @Override
         public void beforeProcess(ExecutorService executor, int coreNum) {
             logger.info(String.format("Start processing the weak form problem by %d parrel assembliers", coreNum));
-            
+
         }
-        
+
         @Override
-        public void processStarted(ExecutorService executor){
-            CountableQuadraturePointIterator balanceIterator=processor.balanceIterator;
-            CountableQuadraturePointIterator dirichletIterator=processor.dirichletIterator;
-            CountableQuadraturePointIterator neumannIterator=processor.neumannIterator;
+        public void processStarted(ExecutorService executor) {
+            QuadraturePointIterator balanceIterator = processor.balanceIterator;
+            QuadraturePointIterator dirichletIterator = processor.dirichletIterator;
+            QuadraturePointIterator neumannIterator = processor.neumannIterator;
             while (!executor.isTerminated()) {
                 try {
                     executor.awaitTermination(100, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException ex) {
                     break;
                 } finally {
-                    logger.info(String.format("Assemblied: balance %d/%d, Dirichlet %d/%d, Neumann %d/%d", balanceIterator.dispatchedNum(), balanceIterator.sunNum(), dirichletIterator.dispatchedNum(), dirichletIterator.sunNum(), neumannIterator.dispatchedNum(), neumannIterator.sunNum()));
+                    logger.info(String.format("Assemblied: balance %d/%d, Dirichlet %d/%d, Neumann %d/%d", balanceIterator.getDispatchedNum(), balanceIterator.getSumNum(), dirichletIterator.getDispatchedNum(), dirichletIterator.getSumNum(), neumannIterator.getDispatchedNum(), neumannIterator.getSumNum()));
                 }
             }
         }
@@ -82,14 +129,143 @@ public class WeakformProcessorMonitors {
         }
 
         @Override
+        public void setProcessor(WeakformProcessor processor) {
+            super.setProcessor(processor);
+            logger = Logger.getLogger(processor.getClass());
+        }
+    }
+
+    public static WeakformProcessorMonitor recorder(){
+        return new Recorder();
+    }
+    
+    public static class Recorder extends Adapter {
+
+        public List<List<List<QuadraturePoint>>> qpRecords;
+        public List<List<List<List<Node>>>> supNdsRecords;
+        public List<List<List<TDoubleArrayList[]>>> shapeFunValsRecords;
+        public static final int BALANCE_IDX = 0, DIRICHLET_IDX = 1, NEUMANN_IDX = 2;
+
+        @Override
+        public void beforeProcess(ExecutorService executor, int coreNum) {
+            super.beforeProcess(executor, coreNum);
+            initRecords(coreNum);
+        }
+
+        private void initRecords(int coreNum) {
+            qpRecords = new ArrayList<>(3);
+            shapeFunValsRecords = new ArrayList<>(3);
+            supNdsRecords = new ArrayList<>(3);
+            for (int i = 0; i < 3; i++) {
+                qpRecords.add(new ArrayList<List<QuadraturePoint>>(coreNum));
+                shapeFunValsRecords.add(new ArrayList<List<TDoubleArrayList[]>>(coreNum));
+                supNdsRecords.add(new ArrayList<List<List<Node>>>());
+                for (int j = 0; j < coreNum; j++) {
+                    qpRecords.get(i).add(new LinkedList<QuadraturePoint>());
+                    shapeFunValsRecords.get(i).add(new LinkedList<TDoubleArrayList[]>());
+                    supNdsRecords.get(i).add(new LinkedList<List<Node>>());
+                }
+            }
+        }
+
+        @Override
+        public void balanceAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, VolumeCondition volBc, int asmId) {
+            super.balanceAsmed(qp, nodes, shapeFunVals, volBc, asmId);
+            List<QuadraturePoint> qpRecord = qpRecords.get(BALANCE_IDX).get(asmId);
+            List<List<Node>> supNdsRecord = supNdsRecords.get(BALANCE_IDX).get(asmId);
+            List<TDoubleArrayList[]> shapeFunRecord = shapeFunValsRecords.get(BALANCE_IDX).get(asmId);
+
+            qpRecord.add(new QuadraturePoint(qp));
+            supNdsRecord.add(new ArrayList<>(nodes));
+            shapeFunRecord.add(CommonUtils.copyTDoubleArrayListArray(shapeFunVals));
+        }
+    }
+
+    public static WeakformProcessorMonitor compact(Collection<WeakformProcessorMonitor> monitors) {
+        return new Compact(monitors);
+    }
+
+    public static class Compact implements WeakformProcessorMonitor {
+
+        List<WeakformProcessorMonitor> monitors;
+
+        public Compact(Collection<WeakformProcessorMonitor> monitors) {
+            this.monitors = new ArrayList<>(monitors);
+        }
+
+        @Override
+        public void avatorInited(WeakformAssemblier asm, ShapeFunction shapeFun, int asmId) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.avatorInited(asm, shapeFun, asmId);
+            }
+        }
+
+        @Override
+        public void balanceAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, VolumeCondition volBc, int asmId) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.balanceAsmed(qp, nodes, shapeFunVals, volBc, asmId);
+            }
+        }
+
+        @Override
+        public void dirichletAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, int asmId) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.dirichletAsmed(qp, nodes, shapeFunVals, asmId);
+            }
+        }
+
+        @Override
+        public void neumannAsmed(QuadraturePoint qp, List<Node> nodes, TDoubleArrayList[] shapeFunVals, int asmId) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.neumannAsmed(qp, nodes, shapeFunVals, asmId);
+            }
+        }
+
+        @Override
+        public void beforeProcess(ExecutorService executor, int coreNum) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.beforeProcess(executor, coreNum);
+            }
+        }
+
+        @Override
+        public void processStarted(ExecutorService executor) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.processStarted(executor);
+            }
+        }
+
+        @Override
+        public void avatorUnited(WeakformAssemblier asm) {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.avatorUnited(asm);
+            }
+        }
+
+        @Override
+        public void beforeEquationSolve() {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.beforeEquationSolve();
+            }
+        }
+
+        @Override
+        public void equationSolved() {
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.equationSolved();
+            }
+        }
+
+        @Override
         public WeakformProcessor getProcessor() {
-            return processor;
+            return monitors.get(0).getProcessor();
         }
 
         @Override
         public void setProcessor(WeakformProcessor processor) {
-            this.processor = processor;
-            logger = Logger.getLogger(processor.getClass());
+            for (WeakformProcessorMonitor monitor : monitors) {
+                monitor.setProcessor(processor);
+            }
         }
     }
 }
--- src/net/epsilony/simpmeshfree/utils/CommonUtils.java
+++ src/net/epsilony/simpmeshfree/utils/CommonUtils.java
@@ -4,6 +4,7 @@
  */
 package net.epsilony.simpmeshfree.utils;
 
+import gnu.trove.list.array.TDoubleArrayList;
 import no.uib.cipr.matrix.Matrix;
 import no.uib.cipr.matrix.MatrixEntry;
 import org.apache.commons.math.util.MathUtils;
@@ -14,20 +15,21 @@ import org.ejml.data.DenseMatrix64F;
  * @author epsilon
  */
 public class CommonUtils {
-    public static int len2DBase(int order){
-        return (order+2)*(order+1)/2;
+
+    public static int len2DBase(int order) {
+        return (order + 2) * (order + 1) / 2;
     }
-    
-    public static int len3DBase(int order){
-         int result=0;
-         for(int i=0;i<=order;i++){
-             result+=MathUtils.binomialCoefficient(2+i, 2);
-         }
-         return result;
+
+    public static int len3DBase(int order) {
+        int result = 0;
+        for (int i = 0; i <= order; i++) {
+            result += MathUtils.binomialCoefficient(2 + i, 2);
+        }
+        return result;
     }
-    
-    public static int lenBase(int dim,int order){
-        switch(dim){
+
+    public static int lenBase(int dim, int order) {
+        switch (dim) {
             case 2:
                 return len2DBase(order);
             case 3:
@@ -36,13 +38,25 @@ public class CommonUtils {
                 throw new UnsupportedOperationException();
         }
     }
-    
-    public static DenseMatrix64F toDenseMatrix64F(Matrix mat){
-        DenseMatrix64F result=new DenseMatrix64F(mat.numRows(),mat.numColumns());
-        for (MatrixEntry me:mat){
-            result.set(me.row(),me.column(),me.get());
+
+    public static DenseMatrix64F toDenseMatrix64F(Matrix mat) {
+        DenseMatrix64F result = new DenseMatrix64F(mat.numRows(), mat.numColumns());
+        for (MatrixEntry me : mat) {
+            result.set(me.row(), me.column(), me.get());
         }
         return result;
     }
-    
+
+    public static TDoubleArrayList[] copyTDoubleArrayListArray(TDoubleArrayList[] ori) {
+        TDoubleArrayList[] res = new TDoubleArrayList[ori.length];
+        for (int i = 0; i < ori.length; i++) {
+            TDoubleArrayList val = ori[i];
+            if (val == null) {
+                res[i] = null;
+            } else {
+                res[i] = new TDoubleArrayList(val);
+            }
+        }
+        return res;
+    }
 }
